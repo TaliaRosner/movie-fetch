3e799dd48270d5c95bbd5802c140189a14ae1ef3
TaliaRosner <taliarosner@gmail.com>
2025-04-29 16:25:50 -0400

    Add GET, POST, DELETE routes and auth token usage

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// index.js
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const Models = require("./models.js");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -17,18 +17,27 @@ mongoose.connect("mongodb://localhost:27017/moviefetch", {
 app.use(morgan("common")); // Logging all requests
 app.use(express.json()); // Parsing JSON request bodies
 app.use(express.static("public")); // Serving static files
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let auth = require("./auth")(app);
+const passport = require("passport");
+require("./passport"); // Importing passport strategies
 
 // Route to return all movies
-app.get("/movies", (req, res) => {
-  Movies.find()
-    .then((movies) => {
-      res.status(200).json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.find()
+      .then((movies) => {
+        res.status(200).json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Route to return one movie by title
 app.get("/movies/:title", (req, res) => {
@@ -68,40 +77,70 @@ app.post("/users", (req, res) => {
 });
 
 // Route to return all users
-app.get("/users", (req, res) => {
-  Users.find()
-    .then((users) => res.status(200).json(users))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/users",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Users.find()
+      .then((users) => res.status(200).json(users))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
+
+// Route to get a user by username
+app.get(
+  "/users/:username",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Users.findOne({ Username: req.params.username })
+      .then((user) => {
+        if (!user) {
+          return res.status(404).send("User not found");
+        }
+        res.status(200).json(user);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Update user info (e.g., username, password, email, birthday)
-app.put("/users/:username", (req, res) => {
-  Users.findOneAndUpdate(
-    { Username: req.params.username },
-    {
-      $set: {
-        Username: req.body.Username,
-        Password: req.body.Password,
-        Email: req.body.Email,
-        Birthday: req.body.Birthday,
+app.put(
+  "/users/:username",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    if (req.user.Username !== req.params.username) {
+      return res.status(400).send("Permission denied");
+    }
+    Users.findOneAndUpdate(
+      { Username: req.params.username },
+      {
+        $set: {
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        },
       },
-    },
-    { new: true }
-  )
-    .then((updatedUser) => {
-      if (!updatedUser) {
-        return res.status(404).send("User not found");
-      }
-      res.status(200).json(updatedUser);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+      { new: true }
+    )
+      .then((updatedUser) => {
+        if (!updatedUser) {
+          return res.status(404).send("User not found");
+        }
+        res.status(200).json(updatedUser);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // TEMP: Add a movie (for testing only)
 app.post("/movies", (req, res) => {
@@ -114,64 +153,85 @@ app.post("/movies", (req, res) => {
 });
 
 // Add movie to user's favorites
-app.post("/users/:username/movies/:movieId", (req, res) => {
-  Users.findOneAndUpdate(
-    { Username: req.params.username },
-    {
-      $addToSet: {
-        FavoriteMovies: new mongoose.Types.ObjectId(req.params.movieId),
+app.post(
+  "/users/:username/movies/:movieId",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    if (req.user.Username !== req.params.username) {
+      return res.status(400).send("Permission denied");
+    }
+    Users.findOneAndUpdate(
+      { Username: req.params.username },
+      {
+        $addToSet: {
+          FavoriteMovies: new mongoose.Types.ObjectId(req.params.movieId),
+        },
       },
-    },
-    { new: true }
-  )
-    .then((updatedUser) => {
-      if (updatedUser) {
-        res.status(200).json(updatedUser);
-      } else {
-        res.status(404).send("User not found");
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+      { new: true }
+    )
+      .then((updatedUser) => {
+        if (updatedUser) {
+          res.status(200).json(updatedUser);
+        } else {
+          res.status(404).send("User not found");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Remove a movie from user's favorites (with Mongoose)
-app.delete("/users/:username/movies/:movieId", (req, res) => {
-  Users.findOneAndUpdate(
-    { Username: req.params.username },
-    { $pull: { FavoriteMovies: req.params.movieId } },
-    { new: true }
-  )
-    .then((updatedUser) => {
-      if (updatedUser) {
-        res.status(200).json(updatedUser);
-      } else {
-        res.status(404).send("User not found");
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.delete(
+  "/users/:username/movies/:movieId",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    if (req.user.Username !== req.params.username) {
+      return res.status(400).send("Permission denied");
+    }
+    Users.findOneAndUpdate(
+      { Username: req.params.username },
+      { $pull: { FavoriteMovies: req.params.movieId } },
+      { new: true }
+    )
+      .then((updatedUser) => {
+        if (updatedUser) {
+          res.status(200).json(updatedUser);
+        } else {
+          res.status(404).send("User not found");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Deregister an existing user (delete user document)
-app.delete("/users/:username", (req, res) => {
-  Users.findOneAndDelete({ Username: req.params.username })
-    .then((deletedUser) => {
-      if (deletedUser) {
-        res.status(200).send(`User ${req.params.username} was deregistered.`);
-      } else {
-        res.status(404).send("User not found");
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.delete(
+  "/users/:username",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    if (req.user.Username !== req.params.username) {
+      return res.status(400).send("Permission denied");
+    }
+    Users.findOneAndDelete({ Username: req.params.username })
+      .then((deletedUser) => {
+        if (deletedUser) {
+          res.status(200).send(`User ${req.params.username} was deregistered.`);
+        } else {
+          res.status(404).send("User not found");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Return genre description by name
 app.get("/genres/:genreName", (req, res) => {
